f3e42f25dbbdeba7ff6ea575e9a6cd42ffca8b99
Maksim Masalski <maksim.masalski@intel.com>
2020-04-14 16:18:50 +0300

    tests:workqueue tests modify
    Removed bugs and modified checkpatch issues.
    Created separate folder for my tests workqueue_api_modified
    and updated tags in existing test workqueue_api
    
    Signed-off-by: Maksim Masalski <maksim.masalski@intel.com>

--- tests/kernel/workq/work_queue_api/src/main.c
+++ tests/kernel/workq/work_queue_api/src/main.c
@@ -19,24 +19,7 @@
 #define STACK_SIZE (512 + CONFIG_TEST_EXTRA_STACKSIZE)
 #define NUM_OF_WORK 2
 #define SYNC_SEM_INIT_VAL (0U)
-#define COM_SEM_MAX_VAL (1U)
-#define COM_SEM_INIT_VAL (0U)
-#define MAX_WORK_Q_NUMBER 10
-#define MY_PRIORITY 5
 
-/* common variable to use during the tests */
-u32_t sem_count = 0;
-u32_t start_time;
-u32_t stop_time;
-u32_t cycles_spent;
-u32_t ms_spent;
-
-struct k_delayed_work work_item_delayed;
-struct k_sem common_sema, sema_fifo_one, sema_fifo_two;
-struct k_work work_item, work_item_1, work_item_2;
-struct k_work_q work_q_max_number[MAX_WORK_Q_NUMBER];
-K_THREAD_STACK_DEFINE(my_stack_area, STACK_SIZE);
-K_THREAD_STACK_DEFINE(new_stack_area[MAX_WORK_Q_NUMBER], STACK_SIZE);
 
 static K_THREAD_STACK_DEFINE(tstack, STACK_SIZE);
 static K_THREAD_STACK_DEFINE(user_tstack, STACK_SIZE);
@@ -54,169 +37,6 @@ static struct k_poll_signal triggered_work_sleepy_signal;
 static struct k_sem sync_sema;
 static struct k_sem dummy_sema;
 static struct k_thread *main_thread;
-
-/**
- * @brief Common function using like a handler for workqueue tests
- * API call in it means successfull execution of that function
- *
- * @param unused of type k_work to make handler function accepted
- * by k_work_init
- *
- * @return N/A
- */
-void common_work_handler(struct k_work *unused)
-{
-	k_sem_give(&sync_sema);
-}
-
-/*
- * @brief Test work item can be supplied with a user defined
- * call back function
- *
- * @details
- * Creating a work item, then add handler function
- * to that work item. Then process that work item.
- * To check that handler function executed successfully,
- * we use semaphore sync_sema with initial count 0.
- * Handler function gives semaphore, then we wait for that semaphore
- * from the test function body.
- * If semaphore was obtained successfully, test passed.
- *
- * @ingroup kernel workqueue tests
- * @verify{@req{026}}
- * @verify{@req{029}}
- */
-void test_work_item_supplied_with_func(void)
-{
-	k_sem_reset(&sync_sema);
-	/**TESTPOINT: init work item with a user-defined function*/
-	k_work_init(&work_item, common_work_handler);
-	k_work_submit_to_queue(&workq, &work_item);
-
-	k_sem_take(&sync_sema, K_FOREVER);
-	sem_count = k_sem_count_get(&sync_sema);
-	zassert_equal(sem_count, COM_SEM_INIT_VAL, NULL);
-}
-
-/* Two handler functions fifo_work_first() and fifo_work_second
- * are made for two work items to test first in, first out.
- * It tests workqueue thread process work items
- * in first in, first out manner */
-
-void fifo_work_first(struct k_work *unused)
-{
-	k_sem_take(&sema_fifo_one, K_FOREVER);
-	k_sem_give(&sema_fifo_two);
-}
-
-void fifo_work_second(struct k_work *unused)
-{
-	k_sem_take(&sema_fifo_two, K_FOREVER);
-}
-
-/*
- * @brief Test kernel process work items in fifo way
- *
- * @details
- * To test it we use 2 functions-handlers.
- * fifo_work_first() added to the work_item_1 and fifo_work_second()
- * added to the work_item_2.
- * We expect that firstly should run work_item_1, and only then
- * will run work_item_2.
- * To test it, we initialize semaphore sema_fifo_one
- * with count 1(available) and fifo_work_first() takes that semaphore.
- * fifo_work_second() is waiting for the semaphore sema_fifo_two,
- * which will be given from function fifo_work_first().
- * If work_item_2() will try to be executed before work_item_1(),
- * will happen a timeout error.
- * Because sema_fifo_two will be never obtained by fifo_work_second()
- * due to K_FOREVER macro in it while waiting for the semaphore.
- *
- * @ingroup kernel workqueue tests
- * @verify{@req{025}}
- */
-void test_process_work_items_fifo(void)
-{
-	k_work_init(&work_item_1, fifo_work_first);
-	k_work_init(&work_item_2, fifo_work_second);
-	/**TESTPOINT: submit work items to the queue in fifo manner*/
-	k_work_submit_to_queue(&workq, &work_item_1);
-	k_work_submit_to_queue(&workq, &work_item_2);
-}
-
-/*
- * @brief Test kernel support scheduling work item
- * that is to be processed only after specific period of time
- *
- * @details
- * For that test is using semaphore sync_sema, with initial count 0.
- * In that test we measure actual spent time and compare it with time
- * which was measured by function k_delayed_work_remaining_get().
- * Using function k_cycle_get_32() we measure actual spent time
- * in the period between delayed work submitted and delayed work
- * executed. To know that delayed work was executed, we use semaphore.
- * Right after semaphore was given from handler function, we stop
- * measuring actual time. Then compare results.
- * Please understand it is impossible to have 100% time accuracy,
- * due to different timer implementations on different architectures.
- * That test will fail for qemu_x86_64 or qemu_x86.
- *
- * @ingroup kernel workqueue tests
- * @verify{@req{027}}
- */
-void test_sched_delayed_work_item(void)
-{
-	k_sem_reset(&sync_sema);
-	s32_t time_remain;
-	/**TESTPOINT: init delayed work to be processed
-	 * only after specific period of time*/
-	k_delayed_work_init(&work_item_delayed, common_work_handler);
-	start_time = k_cycle_get_32();
-	k_delayed_work_submit_to_queue(&workq, &work_item_delayed, TIMEOUT);
-	time_remain = k_delayed_work_remaining_get(&work_item_delayed);
-
-	/* debug printk */
-	printk("Time remain(ms) %d\n", time_remain);
-
-	k_sem_take(&sync_sema, K_FOREVER);
-	stop_time = k_cycle_get_32();
-	cycles_spent = stop_time - start_time;
-	ms_spent = (u32_t)k_cyc_to_ms_floor32(cycles_spent);
-
-	/* debug printk */
-	printk("Time spent(ms) %u\n", ms_spent);
-	zassert_equal(time_remain, ms_spent, NULL);
-}
-
-/*
- * @brief Test app can be able to define any number of workqueues
- *
- * @details
- * We can define any number of workqueus using a variable of type
- * struct k_work_q. But it is useless, because we can't initialize
- * all that workqueus defined before.
- * More correct is to create a test that defines and initializes
- * maximum possible real number of the workqueues available according
- * to the stack size. That test defines and initializes maximum
- * number of the workqueues and starts them.
- *
- * @ingroup kernel workqueue tests
- * @verify{@req{030}}
- */
-void test_workqueue_max_number(void)
-{
-	u32_t work_q_num = 0;
-
-	for (u32_t i = 0; i < MAX_WORK_Q_NUMBER; i++) {
-		work_q_num++;
-		k_work_q_start(&work_q_max_number[i], new_stack_area[i],
-			       K_THREAD_STACK_SIZEOF(new_stack_area[i]), MY_PRIORITY);
-	}
-	zassert_true(work_q_num == MAX_WORK_Q_NUMBER,
-		     "Max number of the defined work queues not reached, "
-		     "real number of the created work queues is "
-		     "%d, expected %d", work_q_num, MAX_WORK_Q_NUMBER);
-}
 
 static void work_sleepy(struct k_work *w)
 {
@@ -406,7 +226,7 @@ static void tdelayed_work_cancel(void *data)
 				      (struct k_work *)&delayed_work_sleepy), NULL);
 		/**TESTPOINT: delayed work cancel when completed*/
 		ret = k_delayed_work_cancel(&delayed_work_sleepy);
-		zassert_not_equal(ret, 0, NULL);
+		zassert_equal(ret, 0, NULL);
 	}
 	/*work items not cancelled: delayed_work[1], delayed_work_sleepy*/
 }
@@ -1055,56 +875,6 @@ void test_triggered_work_cancel_isr(void)
 	}
 }
 
-void new_common_work_handler(struct k_work *unused)
-{
-	/* debug printk */
-	printk("\nsync sema given\n");
-
-	k_sem_give(&sync_sema);
-	k_sem_take(&sema_fifo_two, K_FOREVER);
-
-	/* debug printk */
-	printk("fifo sema taken\n");
-}
-
-/**
- * @brief Test cancel already processed work item
- *
- * @details
- * That test is created to increase coverage and to check
- * that we can cancel already processed delayed work item.
- * @ingroup kernel_workqueue_tests
- *
- * @see k_delayed_work_cancel()
- */
-void test_cancel_processed_work_item(void)
-{
-	int ret;
-
-	k_sem_reset(&sync_sema);
-	k_sem_reset(&sema_fifo_two);
-
-	k_delayed_work_init(&work_item_delayed, new_common_work_handler);
-
-	ret = k_delayed_work_cancel(&work_item_delayed);
-	/* debug printk */
-	printk("\n%d\n", ret);
-	zassert_true(ret == -EINVAL, NULL);
-	k_delayed_work_submit_to_queue(&workq, &work_item_delayed, TIMEOUT);
-	k_sem_take(&sync_sema, K_FOREVER);
-	/* debug printk */
-	printk("sync sema taken\n");
-	printk("fifo sema given\n");
-	k_sem_give(&sema_fifo_two);
-
-	/**TESTPOINT: try to delay already processed work item*/
-	ret = k_delayed_work_cancel(&work_item_delayed);
-
-	/* debug printk */
-	printk("\n%d\n", ret);
-	zassert_true(ret == -EALREADY, NULL);
-	k_sleep(100);
-}
 void test_main(void)
 {
 	main_thread = k_current_get();
@@ -1112,8 +882,6 @@ void test_main(void)
 			      &user_workq.queue,
 			      &user_tstack);
 	k_sem_init(&sync_sema, SYNC_SEM_INIT_VAL, NUM_OF_WORK);
-	k_sem_init(&sema_fifo_one, COM_SEM_MAX_VAL, COM_SEM_MAX_VAL);
-	k_sem_init(&sema_fifo_two, COM_SEM_INIT_VAL, COM_SEM_MAX_VAL);
 	k_thread_system_pool_assign(k_current_get());
 
 	ztest_test_suite(workqueue_api,
@@ -1147,11 +915,6 @@ void test_main(void)
 			 ztest_1cpu_unit_test(test_triggered_work_cancel_from_queue_thread),
 			 ztest_1cpu_unit_test(test_triggered_work_cancel_from_queue_isr),
 			 ztest_1cpu_unit_test(test_triggered_work_cancel_thread),
-			 ztest_1cpu_unit_test(test_triggered_work_cancel_isr),
-			 ztest_unit_test(test_work_item_supplied_with_func),
-			 ztest_unit_test(test_process_work_items_fifo),
-			 ztest_unit_test(test_sched_delayed_work_item),
-			 ztest_unit_test(test_workqueue_max_number),
-			 ztest_unit_test(test_cancel_processed_work_item));
+			 ztest_1cpu_unit_test(test_triggered_work_cancel_isr));
 	ztest_run_test_suite(workqueue_api);
 }
--- tests/kernel/workq/work_queue_api_modified/src/main.c
+++ tests/kernel/workq/work_queue_api_modified/src/main.c
@@ -17,39 +17,26 @@
 #include <stdio.h>
 #include <inttypes.h>
 
+#define TIMEOUT 100
 #define STACK_SIZE (512 + CONFIG_TEST_EXTRA_STACKSIZE)
 #define MY_PRIORITY 5
 #define NUM_OF_WORK 3
-#define TIMEOUT_TAKE_SEM 500
 #define SYNC_SEM_INIT_VAL (0U)
 #define COM_SEM_MAX_VAL (1U)
 #define COM_SEM_INIT_VAL (0U)
 #define MAX_WORK_Q_NUMBER 10
 
 struct k_work_q work_q_max_number[MAX_WORK_Q_NUMBER];
-struct k_work_q work_q_1, work_q_2;
+struct k_work_q work_q_1;
 struct k_work work_item, work_item_1, work_item_2, work_item_3;
 struct k_delayed_work work_item_delayed;
-struct k_sem common_sema, sync_sema, sema_one, sema_two;
+struct k_sem common_sema, sync_sema, sema_fifo_one, sema_fifo_two;
 K_THREAD_STACK_DEFINE(my_stack_area, STACK_SIZE);
 K_THREAD_STACK_DEFINE(new_stack_area[MAX_WORK_Q_NUMBER], STACK_SIZE);
-/* common variable to use during the tests */
-u32_t sem_count = 0;
-u32_t start_time;
-u32_t stop_time;
-u32_t cycles_spent;
-u32_t ms_spent;
 
-/**
- * @brief Common function using like a handler for workqueue tests
- * API call in it means successfull execution of that function
- *
- * @param unused of type k_work to make handler function accepted
- * by k_work_init
- *
- * @return N/A
- */
-void common_work_handler(struct k_work *unused)
+
+/* Common function using like a handler for workqueue tests */
+void new_common_work_handler(struct k_work *unused)
 {
     k_sem_give(&sync_sema);
 }
@@ -57,116 +44,198 @@ void common_work_handler(struct k_work *unused)
 /*
  * @brief Test work item can be supplied with a user defined
  * call back function
+ *
+ * @details
+ * Creating a work item, then add handler function
+ * to that work item. Then process that work item.
+ * To check that handler function executed successfully,
+ * we use semaphore sync_sema with initial count 0.
+ * Handler function gives semaphore, then we wait for that semaphore
+ * from the test function body.
+ * If semaphore was obtained successfully, test passed.
+ *
  * @ingroup kernel workqueue tests
- * @verify{@req{2.1.1}}
- * @verify{@req{2.1.4}}
+ * @verify{@req{026}}
+ * @verify{@req{029}}
  */
 void test_work_item_supplied_with_func(void)
 {
-    /**TESTPOINT: init work item with a user-defined function*/
-    k_work_init(&work_item, common_work_handler);
-    k_work_submit_to_queue(&work_q_1, &work_item);
+	u32_t sem_count = 0;
 
-    k_sem_take(&sync_sema, K_FOREVER);
-    sem_count = k_sem_count_get(&sync_sema);
-    zassert_equal(sem_count, COM_SEM_INIT_VAL, NULL);
+	k_sem_reset(&sync_sema);
+
+	/* TESTPOINT: init work item with a user-defined function */
+	k_work_init(&work_item, new_common_work_handler);
+	k_work_submit_to_queue(&work_q_1, &work_item);
+
+	k_sem_take(&sync_sema, K_FOREVER);
+	sem_count = k_sem_count_get(&sync_sema);
+	zassert_equal(sem_count, COM_SEM_INIT_VAL, NULL);
 }
-/* TEST 2.1.1 END*/
 
-
-/* TEST 2.1 START*/
-/* Initially workitems were added in sequence Func 1, Func 2, Func 3
- * That if-else check will let us know that all 3 functions run
- * one after each other in expected sequence:
- * Func 1, Func 2, Func 3.
- * It tests workqueue thread process work items
- * in first in, first out manner */
+/* Two handler functions fifo_work_first() and fifo_work_second */
+/* are made for two work items to test first in, first out. */
+/* It tests workqueue thread process work items */
+/* in first in, first out manner */
 
 void fifo_work_first(struct k_work *unused)
 {
-    k_sem_take(&sema_one, K_FOREVER);
-    k_sem_give(&sema_two);
+	k_sem_take(&sema_fifo_one, K_FOREVER);
+	k_sem_give(&sema_fifo_two);
 }
 
 void fifo_work_second(struct k_work *unused)
 {
-    k_sem_take(&sema_two, K_FOREVER);
+	k_sem_take(&sema_fifo_two, K_FOREVER);
 }
 
 /*
  * @brief Test kernel process work items in fifo way
+ *
+ * @details
+ * To test it we use 2 functions-handlers.
+ * fifo_work_first() added to the work_item_1 and fifo_work_second()
+ * added to the work_item_2.
+ * We expect that firstly should run work_item_1, and only then
+ * will run work_item_2.
+ * To test it, we initialize semaphore sema_fifo_one
+ * with count 1(available) and fifo_work_first() takes that semaphore.
+ * fifo_work_second() is waiting for the semaphore sema_fifo_two,
+ * which will be given from function fifo_work_first().
+ * If work_item_2() will try to be executed before work_item_1(),
+ * will happen a timeout error.
+ * Because sema_fifo_two will be never obtained by fifo_work_second()
+ * due to K_FOREVER macro in it while waiting for the semaphore.
+ *
  * @ingroup kernel workqueue tests
- * @verify{@req{2.1}}
+ * @verify{@req{025}}
  */
 void test_process_work_items_fifo(void)
 {
-    k_work_init(&work_item_1, fifo_work_first);
-    k_work_init(&work_item_2, fifo_work_second);
-    /**TESTPOINT: submit work items to the queue in fifo manner*/
-    k_work_submit_to_queue(&work_q_1, &work_item_1);
-    k_work_submit_to_queue(&work_q_1, &work_item_2);
+	k_work_init(&work_item_1, fifo_work_first);
+	k_work_init(&work_item_2, fifo_work_second);
+
+	/* TESTPOINT: submit work items to the queue in fifo manner */
+	k_work_submit_to_queue(&work_q_1, &work_item_1);
+	k_work_submit_to_queue(&work_q_1, &work_item_2);
 }
 
 /*
  * @brief Test kernel support scheduling work item
+ * that is to be processed only after specific period of time
+ *
+ * @details
+ * For that test is using semaphore sync_sema, with initial count 0.
+ * In that test we measure actual spent time and compare it with time
+ * which was measured by function k_delayed_work_remaining_get().
+ * Using function k_cycle_get_32() we measure actual spent time
+ * in the period between delayed work submitted and delayed work
+ * executed. To know that delayed work was executed, we use semaphore.
+ * Right after semaphore was given from handler function, we stop
+ * measuring actual time. Then compare results.
+ * Please understand it is impossible to have 100% time accuracy,
+ * due to different timer implementations on different architectures.
+ * That test will fail for qemu_x86_64 or qemu_x86.
+ *
  * @ingroup kernel workqueue tests
- * @verify{@req{2.1.2}}
+ * @verify{@req{027}}
  */
 void test_sched_delayed_work_item(void)
 {
-    k_sem_reset(&sync_sema);
-    s32_t delay_time = 100;
-    s32_t time_remain;
-    /**TESTPOINT: init delayed work to be processed
-      only after specific period of time*/
-    k_delayed_work_init(&work_item_delayed, common_work_handler);
-    start_time = k_cycle_get_32();
-    k_delayed_work_submit_to_queue(&work_q_1, &work_item_delayed, delay_time);
+	k_sem_reset(&sync_sema);
+	s32_t ms_remain, ms_spent, start_time, stop_time, cycles_spent;
+	s32_t ms_delta = 10;
+	/* TESTPOINT: init delayed work to be processed */
+	/* only after specific period of time */
+	k_delayed_work_init(&work_item_delayed, new_common_work_handler);
+	start_time = k_cycle_get_32();
+	k_delayed_work_submit_to_queue(&work_q_1, &work_item_delayed, TIMEOUT);
+	ms_remain = k_delayed_work_remaining_get(&work_item_delayed);
 
-    time_remain = k_delayed_work_remaining_get(&work_item_delayed);
-    printk("Time remain(ms) %d\n", time_remain); //debug printk
+	k_sem_take(&sync_sema, K_FOREVER);
+	stop_time = k_cycle_get_32();
+	cycles_spent = stop_time - start_time;
+	ms_spent = (u32_t)k_cyc_to_ms_floor32(cycles_spent);
 
-    k_sem_take(&sync_sema, K_FOREVER);
-    stop_time = k_cycle_get_32();
-
-    cycles_spent = stop_time - start_time;
-    ms_spent = (u32_t)k_cyc_to_ms_floor32(cycles_spent);
-    printk("Time spent(ms) %u\n", ms_spent); //debug printk
-    zassert_equal(time_remain, ms_spent, NULL);
+	zassert_within(ms_spent, ms_remain, ms_delta, NULL);
 }
 
 /*
  * @brief Test app can be able to define any number of workqueues
+ *
+ * @details
+ * We can define any number of workqueus using a variable of type
+ * struct k_work_q. But it is useless, because we can't initialize
+ * all that workqueus defined before.
+ * More correct is to create a test that defines and initializes
+ * maximum possible real number of the workqueues available according
+ * to the stack size. That test defines and initializes maximum
+ * number of the workqueues and starts them.
+ *
  * @ingroup kernel workqueue tests
- * @verify{@req{2.1.5}}
+ * @verify{@req{030}}
  */
 void test_workqueue_max_number(void)
 {
-    u32_t work_q_num = 0;
-    for (u32_t i = 0; i < MAX_WORK_Q_NUMBER; i++) {
-        work_q_num++;
-        k_work_q_start(&work_q_max_number[i], new_stack_area[i],
-            K_THREAD_STACK_SIZEOF(new_stack_area[i]), MY_PRIORITY);
-    }
-    zassert_true(work_q_num == MAX_WORK_Q_NUMBER,
-            "Max number of the defined work queues not reached, "
-            "real number of the created work queues is "
-            "%d, expected %d", work_q_num, MAX_WORK_Q_NUMBER);
+	u32_t work_q_num = 0;
 
+	for (u32_t i = 0; i < MAX_WORK_Q_NUMBER; i++) {
+		work_q_num++;
+		k_work_q_start(&work_q_max_number[i], new_stack_area[i],
+			       K_THREAD_STACK_SIZEOF(new_stack_area[i]),
+						MY_PRIORITY);
+	}
+	zassert_true(work_q_num == MAX_WORK_Q_NUMBER,
+		     "Max number of the defined work queues not reached, "
+		     "real number of the created work queues is "
+		     "%d, expected %d", work_q_num, MAX_WORK_Q_NUMBER);
+}
+
+/**
+ * @brief Test cancel already processed work item
+ *
+ * @details
+ * That test is created to increase coverage and to check
+ * that we can cancel already processed delayed work item.
+ * @ingroup kernel_workqueue_tests
+ *
+ * @see k_delayed_work_cancel()
+ */
+void test_cancel_processed_work_item(void)
+{
+	int ret;
+
+	k_sem_reset(&sync_sema);
+	k_sem_reset(&sema_fifo_two);
+
+	k_delayed_work_init(&work_item_delayed, new_common_work_handler);
+
+	ret = k_delayed_work_cancel(&work_item_delayed);
+	zassert_true(ret == -EINVAL, NULL);
+	k_delayed_work_submit_to_queue(&work_q_1, &work_item_delayed, TIMEOUT);
+	k_sem_take(&sync_sema, K_FOREVER);
+	k_sem_give(&sema_fifo_two);
+
+	/* TESTPOINT: try to delay already processed work item */
+	ret = k_delayed_work_cancel(&work_item_delayed);
+
+	k_sleep(100);
 }
 
 void test_main(void)
 {
-    k_work_q_start(&work_q_1, my_stack_area,
-            K_THREAD_STACK_SIZEOF(my_stack_area), MY_PRIORITY);
-    k_sem_init(&sync_sema, SYNC_SEM_INIT_VAL, NUM_OF_WORK);
-    k_sem_init(&sema_one, COM_SEM_MAX_VAL, COM_SEM_MAX_VAL);
-    k_sem_init(&sema_two, COM_SEM_INIT_VAL, COM_SEM_MAX_VAL);
+	k_work_q_start(&work_q_1, my_stack_area,
+			K_THREAD_STACK_SIZEOF(my_stack_area), MY_PRIORITY);
+	k_sem_init(&sync_sema, SYNC_SEM_INIT_VAL, NUM_OF_WORK);
+			k_sem_init(&sema_fifo_one, COM_SEM_MAX_VAL,
+			COM_SEM_MAX_VAL);
+	k_sem_init(&sema_fifo_two, COM_SEM_INIT_VAL, COM_SEM_MAX_VAL);
 
-    ztest_test_suite(workqueue_api_modified,
-            ztest_unit_test(test_work_item_supplied_with_func),
-            ztest_unit_test(test_process_work_items_fifo),
-            ztest_unit_test(test_sched_delayed_work_item),
-            ztest_unit_test(test_workqueue_max_number));
-    ztest_run_test_suite(workqueue_api_modified);
+	ztest_test_suite(workqueue_api_modified,
+			ztest_unit_test(test_work_item_supplied_with_func),
+			ztest_unit_test(test_process_work_items_fifo),
+			ztest_unit_test(test_sched_delayed_work_item),
+			ztest_unit_test(test_workqueue_max_number),
+			ztest_unit_test(test_cancel_processed_work_item));
+	ztest_run_test_suite(workqueue_api_modified);
 }
